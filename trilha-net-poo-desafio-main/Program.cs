608367014a253e6551e7be9241c262f1200f4d34
João Vitor <62443308+joaovianaAlves@users.noreply.github.com>
2024-01-11 14:59:14 -0300

    Add files via upload

---
+++ trilha-net-poo-desafio-main/Program.cs
@@ -0,0 +1,7 @@
+﻿using DesafioPOO.Models;
+
+Nokia nokia = new Nokia("13981028934", "A01", "350925378563732", 32);
+Iphone iphone = new Iphone("13981028934", "M14", "350925378563732", 128);
+
+nokia.InstalarAplicativo("TesteNokia");
+iphone.InstalarAplicativo("TesteIphone");
