cdf58e5170743710afc7370cd4a9c3a08d66dfae
João Vitor <62443308+joaovianaAlves@users.noreply.github.com>
2024-01-11 15:04:01 -0300

    Add files via upload

--- trilha-net-poo-desafio-main/Program.cs
+++ trilha-net-poo-desafio-main/Program.cs
@@ -1,7 +1,9 @@
 ﻿using DesafioPOO.Models;
 
 Nokia nokia = new Nokia("13981028934", "A01", "350925378563732", 32);
-Iphone iphone = new Iphone("13981028934", "M14", "350925378563732", 128);
-
 nokia.InstalarAplicativo("TesteNokia");
+nokia.Ligar();
+
+Iphone iphone = new Iphone("13981028934", "M14", "350925378563732", 128);
 iphone.InstalarAplicativo("TesteIphone");
+iphone.ReceberLigacao();
