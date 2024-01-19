13b9d0e11833e39af7aca6c6090810d752423d04
João Vitor <62443308+joaovianaAlves@users.noreply.github.com>
2024-01-19 13:13:41 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using DesafioPOO.Models;
+
+Nokia nokia = new Nokia("13981028934", "A01", "350925378563732", 32);
+nokia.InstalarAplicativo("TesteNokia");
+nokia.Ligar();
+
+Iphone iphone = new Iphone("13981028934", "M14", "350925378563732", 128);
+iphone.InstalarAplicativo("TesteIphone");
+iphone.ReceberLigacao();
