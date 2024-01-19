8a7fb49d2c2c696aa51a85a9ce55de83dab1b960
João Vitor <62443308+joaovianaAlves@users.noreply.github.com>
2024-01-19 13:13:58 -0300

    Delete trilha-net-poo-desafio-main directory

--- trilha-net-poo-desafio-main/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using DesafioPOO.Models;
-
-Nokia nokia = new Nokia("13981028934", "A01", "350925378563732", 32);
-nokia.InstalarAplicativo("TesteNokia");
-nokia.Ligar();
-
-Iphone iphone = new Iphone("13981028934", "M14", "350925378563732", 128);
-iphone.InstalarAplicativo("TesteIphone");
-iphone.ReceberLigacao();
